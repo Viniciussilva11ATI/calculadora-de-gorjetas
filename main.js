cf2e77a67e8fe0eca46cb37994a54c44affb860e
Vinicius Silva <vincius_silva@eaportal.org>
2025-03-31 20:27:10 -0300

    Criação da logica de selecionar um botão de gorjeta ou colocar um valor customizado

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ function receiveNumberOfPeopleValue() {
 function receiveTipPercententageValue(value) {
     tipPercentage = value / 100
 
+     document.querySelector("#custom-tip").value = ""
+
     if (buttonSelected !== null) {
         buttonSelected.classList.remove("button-selected")
     }
@@ -21,3 +23,19 @@ function receiveTipPercententageValue(value) {
     buttonSelected = document.querySelector('#button-${value}')
     buttonSelected.classList.add("button-selected")
 }
+
+    function receiveCustomTipPercentageValue() {
+        tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+
+        removeClassButtonSelected()
+    }
+
+    function removeClassButtonSelected() {
+        if (buttonSelected !== null) {
+            buttonSelected.classList.remove("button-selected")
+            buttonSelected = null
+        }
+    }
+  
+        
+
